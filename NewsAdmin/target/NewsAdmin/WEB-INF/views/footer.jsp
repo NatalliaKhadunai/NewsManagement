d5e25f632c8c7020f015a16fef54a1cae42d0e77
NatalliaKhadunai <hodunay2009@yandex.ru>
2016-10-20 19:46:10 +0300

    target dirs removed

--- NewsAdmin/target/NewsAdmin/WEB-INF/views/footer.jsp
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    <p>
-        Copyright @ Epam, 2016
-    </p>
-</footer>
--- NewsAdmin/target/NewsAdmin/resources/js/TagFilter.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#tagSelect').click(function(){
-        $('#checkboxes').toggle();
-        $('#checkboxes').css({'position': 'absolute', 'background-color': 'white'});
-    });
-});
